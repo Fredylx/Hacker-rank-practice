e9fe8d9bd3a54d7db4e484355f048d7a1722b7c4
Fred Lopez <84998401+Fredylx@users.noreply.github.com>
2022-08-30 09:50:55 -0700

    Update Lonely-Integer.py

--- Lonely-Integer.py
+++ Lonely-Integer.py
@@ -12,9 +12,18 @@ import sys
 # The function is expected to return an INTEGER.
 # The function accepts INTEGER_ARRAY a as parameter.
 #
+# logic
+# same input = 0
+# diff input = 1
+# 1 ^ 0 = 1
+# 1 ^ 1 = 1
+# A ^ A = 0
 
 def lonelyinteger(a):
-    # Write your code here
+    result = 0          # set a result starting at 0
+    for num in a:       # parse through a
+        result ^= num   # follow logic
+    return result       # return
 
 if __name__ == '__main__':
     fptr = open(os.environ['OUTPUT_PATH'], 'w')
