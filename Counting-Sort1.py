2598e84b5ba94073c276300d04e85fdb5143aa10
Fred Lopez <84998401+Fredylx@users.noreply.github.com>
2022-08-30 20:25:42 -0700

    Update Counting-Sort1.py

--- Counting-Sort1.py
+++ Counting-Sort1.py
@@ -1,1 +1,49 @@
+#!/bin/python3
+
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'countingSort' function below.
+#
+# The function is expected to return an INTEGER_ARRAY.
+# The function accepts INTEGER_ARRAY arr as parameter.
+#
+
+def countingSort(arr):
+    # 
+    count = [0] * 100           #contraint from numbers 0 - 99, create an arr of zero and * by 100
+    
+    for num in arr:             #iterate through the array
+        count[num] += 1         #find the index of the number and add 1
+    
+    return count
+    
+    
+    
+    
+    
+    
+    
+    
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    n = int(input().strip())
+
+    arr = list(map(int, input().rstrip().split()))
+
+    result = countingSort(arr)
+
+    fptr.write(' '.join(map(str, result)))
+    fptr.write('\n')
+
+    fptr.close()
+
+
 
