1a039c4baa4e2bb625847f1add22234791155a24
Fred Lopez <84998401+Fredylx@users.noreply.github.com>
2022-08-31 09:14:07 -0700

    Update Time-Conversion.py

--- Time-Conversion.py
+++ Time-Conversion.py
@@ -1,1 +1,32 @@
+# Time Conversion
+#Given a time in -hour AM/PM format, convert it to military (24-hour) time.
+#
+#!/bin/python3
 
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'timeConversion' function below.
+#
+# The function is expected to return a STRING.
+# The function accepts STRING s as parameter.
+#
+
+def timeConversion(s):
+    # 
+    
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    s = input()
+
+    result = timeConversion(s)
+
+    fptr.write(result + '\n')
+
+    fptr.close()
