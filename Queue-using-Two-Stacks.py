092d1750ec5889d9a8d97bca5900157140e2041e
Fred Lopez <84998401+Fredylx@users.noreply.github.com>
2022-09-02 00:10:20 -0700

    Update Queue-using-Two-Stacks.py

--- Queue-using-Two-Stacks.py
+++ Queue-using-Two-Stacks.py
@@ -1,1 +1,24 @@
 
+# stackpush - 
+# stackdelete -
+
+
+q = int(input())
+stackpush = []
+stackdelete = []
+for i in range(q):
+  t = list(input().split())
+  # equeue
+  if t[0] == '1':
+    stackpush.append(t[1])
+    
+    
+  # dequeue
+  elif t[0] == '2':
+    if not stackdelete:
+      while stackpush:
+        stackdelete.append(stackpuush.pop())
+    stackdelete.pop()
+  
+  
+    
