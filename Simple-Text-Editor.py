05553ea4fde4ab84fae57a9156ede74c537126c1
Fred Lopez <84998401+Fredylx@users.noreply.github.com>
2022-09-04 01:21:54 -0700

    Create Simple-Text-Editor.py

---
+++ Simple-Text-Editor.py
@@ -0,0 +1,20 @@
+# Enter your code here. Read input from STDIN. Print output to STDOUT
+stack = []
+string = ""
+# the input to the string 
+for _ in range(int(input())):
+    t = input().split()
+    # check for query type
+    # append the string
+    if t[0] == '1':
+        stack.append(string)
+        string += t[1]
+    elif t[0] =='2':
+        stack.append(string)
+        string = string[:-int(t[1])]
+    # print char of the string
+    elif t[0] == '3':
+        print(string[int(t[1])-1]) # minus 1 because the stack starts at 0
+        # undo operation
+    elif t[0] =='4':
+        string = stack.pop()
