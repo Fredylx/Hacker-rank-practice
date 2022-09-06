1ad2be644dafaf06cdb381a8522a6f1aa96cf75b
Fred Lopez <84998401+Fredylx@users.noreply.github.com>
2022-09-06 00:59:04 -0700

    Create whatIsTheOutput.java

---
+++ whatIsTheOutput.java
@@ -0,0 +1,9 @@
+public class MyClass {
+    public static void main(String args[]) {
+        int mask = 0x000F;
+        int value = 0x2222;
+        System.out.println(value & mask);
+    }
+}
+
+/* 2*/
