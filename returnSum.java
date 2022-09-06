b63732f24464c34ec0dda9a53f8a1e674dfca063
Fred Lopez <84998401+Fredylx@users.noreply.github.com>
2022-09-06 01:11:38 -0700

    Create returnSum.java

---
+++ returnSum.java
@@ -0,0 +1,14 @@
+String[] sNums = jTextField1.getText().replaceAll("[^1-9]", "").split("(?<!^)");
+int sum = 0;
+for (String s : sNums) {
+    sum += Integer.parseInt(s); // add all digits
+}
+
+while (sum > 9) { // add all digits of the number, until left with one-digit number
+    int temp = 0;
+    while (sum > 0) {
+        temp += sum % 10;
+        sum = sum / 10;
+    }
+    sum = temp;
+}
