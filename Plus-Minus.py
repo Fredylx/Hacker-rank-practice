c08e067c8d738d1ff6d8fe2f0df317301468f814
Fred Lopez <84998401+Fredylx@users.noreply.github.com>
2022-09-09 02:35:23 -0700

    Update Plus-Minus.py

--- Plus-Minus.py
+++ Plus-Minus.py
@@ -1,3 +1,10 @@
+"""
+Title     : Plus Minus
+Domain    : Python
+Author    : Fredy
+"""
+
+
 #!/bin/python3
 # Given an array of integers, calculate the ratios of its elements that are positive, negative, and zero. Print the decimal value of each fraction on a new line with  places after the decimal.
 #Plus Minus
