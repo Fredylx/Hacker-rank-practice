485451f3baa65d2c0846cd8258e6f2ad3656c7b2
Fred Lopez <84998401+Fredylx@users.noreply.github.com>
2022-09-19 18:04:01 -0700

    Create Birthday Candles Cake.py

---
+++ Birthday Candles Cake.py
@@ -0,0 +1,15 @@
+'''
+Title:      Birthday Candles Cake
+Type:       Warm up
+
+--------------------------------------------------- Solution ---------------------------------------------------
+'''
+
+
+def birthdayCakeCandles(candles):
+    # Write your code here
+    # find the highest number
+    # find the number of times this occures
+    # print out num of occurances of highest #
+    
+    return candles.count(max(candles))
